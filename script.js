3d0df7d8613d6e2e83e6a6b3991a151757fd3e09
Chayan Chandan das <chayanchandandas@gmail.com>
2022-11-07 20:08:51 +0600

    complate css

---
+++ script.js
@@ -0,0 +1,7 @@
+const timeEl = document.getElementById("time");
+const dateEl = document.getElementById("date");
+const currentWeatherItemsEl = document.getElementById("current-weather-items");
+const timeZoneEl = document.getElementById("time-zone");
+const countryEl = document.getElementById('country')
+const crreuntTempEl = document.getElementById('crreunt-temp')
+const weatherForecastEl = document.getElementById('weather-forecast')
